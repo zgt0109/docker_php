7496b311c90b65426dd268ba54964e1e6e637d88
zgt0109 <2506526350@qq.com>
2017-03-31 14:47:33 +0800

    read

--- lib/readmsg.php
+++ lib/readmsg.php
@@ -3,5 +3,20 @@
   // echo "你想看".$tid."行留言";
   $fh = fopen('./msg.txt', 'r');
 
-  
+  $i = 1;
+
+  while (($row = fgetcsv($fh)) != false) {
+    # code...
+    if ($i ==$tid) {
+      # code...
+      // print_r($row);
+      echo "<h1>".$row[0]."</h1>";
+      echo "<p>".$row[1]."</p>";
+    }
+    $i += 1;
+  }
+  // print_r(fgetcsv($fh));
+  // print_r(fgetcsv($fh));
+
+  echo "读取成功";
  ?>
