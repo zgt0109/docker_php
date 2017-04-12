d9b53750a609dd561381e9d1a4924407e3adf1b0
zgt0109 <2506526350@qq.com>
2017-04-12 11:18:47 +0800

    add tp5

--- lib/my.php
+++ lib/my.php
@@ -12,11 +12,58 @@
   // fwrite($fh, "Hello World"."\n");
   // fclose($fh);
   // echo "OK";
-  print_r($_POST);
-  $str = $_POST['title'].",".$_POST['content']."\n";
+  // print_r($_POST);
+  // $str = $_POST['title'].",".$_POST['content']."\n";
+  //
+  // $fh = fopen('./msg.txt', 'a');
+  // fwrite($fh, $str);
+  // fclose($fh);
+  echo "留言成功"."\n";
+  $a ="aa";
+  $aa = "bb";
+  echo $$a."\n";
+  echo "<br>";
 
-  $fh = fopen('./msg.txt', 'a');
-  fwrite($fh, $str);
-  fclose($fh);
-  echo "留言成功";
+  function print_A(){
+    $A = "AAAAAA";
+    echo "A值为:".$A."<p>";
+    // return($A);
+  }
+  // print_A();
+  $B = print_A();
+  echo "B的值是：".$B."<p>";
+  echo "<br>";
+
+  $attr = array("0"=> "aa", "1"=>"bb","2"=>"cc");
+  echo $attr['2'];
+  echo "<br>";
+
+  $x =1;
+  ++$x;
+  $y = $x++;
+  echo $y;
+  echo "<br>";
+
+  $a = array("0"=> "aa", "1"=>"bb","2"=>"cc");
+  echo implode('-', $a);
+  echo "<br>";
+
+  $str = 'hello,world,heiheihei,php';
+  $arr = explode(',', $str);
+  print_r($arr);
+
+  echo "<br>";
+
+  date_default_timezone_set('asia/shanghai');
+  echo date("Y-m-d H:i:s");
+  echo "<br>";
+  echo time("Y-m-d H:i:s")."<br>";
+
+  $attr = array(1,2,3,4);
+  while(list($key,$value) = each($attr)){
+     echo $key."=>".$value."<br>";
+  }
+  while(list($key,$value) = each($attr)){
+     echo $key."=>".$value."<br>";
+  }
  ?>
